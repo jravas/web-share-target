7ac4ecef25bc766f876e1d9dd056cd04734b7d18
jravas <josip.ravas.broj@gmail.com>
2020-04-26 12:22:45 +0200

    Updates

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = "my-site-cache-v1";
+const urlsToCache = [
+  "./main.js",
+  "./images/logo512.png",
+  "./images/logo192.png",
+];
+
+const broadcast = new BroadcastChannel("sw-channel");
+
+self.addEventListener("install", (event) => {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Opened cache", cache);
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", function (event) {
+  if (event.request.method === "POST") {
+    event.respondWith(
+      (async () => {
+        const formData = await event.request.formData();
+        const link = formData.get("file") || "";
+        broadcast.postMessage({ payload: link });
+      })()
+    );
+  }
+  event.respondWith(
+    caches.match(event.request).then(async (response) => {
+      try {
+        return await fetch(event.request);
+      } catch (error) {
+        return response;
+      }
+    })
+  );
+});
