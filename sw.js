47079746e23ab4c92df78495c120c579e7b21c07
jravas <josip.ravas.broj@gmail.com>
2020-04-26 22:31:01 +0200

    Updates

--- sw.js
+++ sw.js
@@ -25,10 +25,9 @@ self.addEventListener("fetch", function (event) {
         const formData = await event.request.formData();
         const link = formData.get("file") || "";
         self.clients.matchAll().then(function (clients) {
-          clients.forEach(function async (client) {
+          clients.forEach(function async(client) {
             console.log(client);
-            client.postMessage(link);
-            return await fetch("index.html");
+            return new Response("Bookmark saved: " + link);
           });
         });
       })()
